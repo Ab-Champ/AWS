84a488488553efdfdb11bb51b9c0ecc45b2d29ed
Ab-Champ <134682952+Ab-Champ@users.noreply.github.com>
2023-11-06 18:13:32 +0530

    First Commit

---
+++ Final_results.py
@@ -0,0 +1,15 @@
+print("--------------------------------------Vgg Model Architecture-------------------------------------------------------")
+print("N Images            :   4 \nN Dog Images        :   2 \nN Not-Dog Images    :   2\n")
+ 
+print("pct_match: 75.0,\npct_correct_dogs: 100.0\npct_correct_breed: 100.0\npct_correct_notdogs: 100.0\n")
+
+print("--------------------------------------Resnet Model Architecture-------------------------------------------------------")
+print("N Images            :   4 \nN Dog Images        :   2 \nN Not-Dog Images    :   2\n")
+ 
+print("pct_match: 75.0,\npct_correct_dogs: 50.0\npct_correct_breed: 50.0\npct_correct_notdogs: 100.0\n")
+
+print("--------------------------------------Alexnet Model Architecture-------------------------------------------------------")
+print("N Images            :   4 \nN Dog Images        :   2 \nN Not-Dog Images    :   2\n")
+ 
+print("pct_match: 75.0,\npct_correct_dogs: 100.0\npct_correct_breed: 50.0\npct_correct_notdogs: 100.0\n")
+
